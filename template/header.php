26b076005d1645d12095ddb5da70f5fbf292608e
KelasQ <66905559+KelasQ@users.noreply.github.com>
2020-06-14 18:22:52 +0700

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,10 @@
+<?php require_once ('config/api.php'); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title><?= $titleChannel ?></title>
+  </head>
+  <body class="bg-light">
